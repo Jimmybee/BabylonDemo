b58edbbec462e023f83aa1b1397f3067b6c0d77c
James Birtwell <jsbirtwell@gmail.com>
2017-07-11 20:06:33 +0100

    Initial Commit

---
+++ BabylonPosts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabylonPosts
+//
+//  Created by James Birtwell on 11/07/2017.
+//  Copyright © 2017 James Birtwell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
